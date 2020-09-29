11ccfc2689bb9daa7d31f0fe022345ec806031f2
Tom <t.rakoto76@gmail.com>
2020-09-29 08:30:53 +0200

    :sparkle: add character animation

--- Valhalla/Assets/Scripts/PlayerController.cs
+++ Valhalla/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 
+public enum Thrust {HIGH, MEDIUM, LOW}
+
 [RequireComponent(typeof(Character))]
 public class PlayerController : MonoBehaviour
 {
@@ -7,6 +9,9 @@ public class PlayerController : MonoBehaviour
     private float _direction;
     private bool _isCrouching;
     private bool _isJumping;
+    private bool _noSword;
+    private bool _throwSword;
+    private Thrust _thrust;
 
     private void Awake()
     {
@@ -19,11 +24,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump")){_isJumping = true;}
         if (Input.GetButtonDown("Crouch")){_isCrouching = true;}
         else if (Input.GetButtonUp("Crouch")){_isCrouching = false;}
+        if (Input.GetButtonDown("Fire1")){_thrust = Thrust.HIGH;}
+        else if (Input.GetButtonDown("Fire2")){_thrust = Thrust.MEDIUM;}
+        else if (Input.GetButtonDown("Fire3")){_thrust = Thrust.LOW;}
+        if (Input.GetButtonDown("Mouse X")){_throwSword = true;}
     }
 
     private void FixedUpdate()
     {
         _character.Move(_direction,_isCrouching,_isJumping);
         _isJumping = false;
+        if (_throwSword)
+        {
+            _character.ThrowSword();
+            _noSword = true;
+        }
+        if(_noSword)  return;
+        _character.Thrusting(_thrust);
     }
 }
