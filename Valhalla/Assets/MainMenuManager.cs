345ffefc3102b9e970d3ef4241c75761fdc2ea03
Antoine Pavy <antoine.pavy92@gmail.com>
2020-09-29 14:34:34 +0200

    :sparkles: Added MainMenuManager

---
+++ Valhalla/Assets/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject mainLayout;
+   
+    public void StartGame()
+    {
+        
+    }
+    
+    public void ShowGameConfigurationScreen()
+    {
+        mainLayout.SetActive(false);
+    }
+
+    public void BackToMainScreen()
+    {
+        mainLayout.SetActive(true);
+    }
+    
+    public void QuitGame()
+    {
+    
+    }
+}
