1f14cf835bcda35e36dc896cf7c2ed806a2966ed
Antoine Pavy <antoine.pavy92@gmail.com>
2020-09-29 21:55:42 +0200

    :recycle: Modified VS show condition, player type getters & StartGame()

--- Valhalla/Assets/MainMenuManager.cs
+++ Valhalla/Assets/MainMenuManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
@@ -10,29 +11,44 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private GameObject mainLayout;
     [SerializeField] private GameObject setupLayout;
     [SerializeField] private Button  VSButton;
-    
-    public enum PlayerType{player,random,mcts,none}
+    [SerializeField] private int  gameSceneIndex = 1;
 
-    public PlayerType player1Type;
-    public PlayerType player2Type;
+    public PlayerType[] playerTypes;
 
     private void Start()
     {
-        player1Type = PlayerType.none;
-        player2Type = PlayerType.none;
+        playerTypes = new PlayerType[GameManager.Instance.NbPlayers];
         ShowMainScreen();
     }
 
     private void Update()
     {
-        if (player1Type != PlayerType.none && player2Type != PlayerType.none)
+        bool readyToPlay = true;
+        foreach (var playerType in playerTypes)
         {
+            if (playerType == PlayerType.none)
+            {
+                readyToPlay = false;
+                break;
+            }
+        }
+
+        if (readyToPlay){
             VSButton.interactable = true;
         }
     }
     public void StartGame()
     {
-        Debug.Log(player1Type + " VS " + player2Type);
+        string log = playerTypes[0].ToString();
+        for (int i = 1; i < playerTypes.Length; ++i)
+        {
+            log += " VS " + playerTypes[i].ToString();
+        }
+
+        if (GameManager.Instance.InitGame(playerTypes))
+        {
+            SceneManager.LoadScene(gameSceneIndex);
+        }
     }
 
     public void ShowGameConfigurationScreen()
@@ -52,10 +68,10 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void SetPlayer1Player(){player1Type = PlayerType.player;}
-    public void SetPlayer1MCTS(){player1Type = PlayerType.mcts;}
-    public void SetPlayer1Random(){player1Type = PlayerType.random;}
-    public void SetPlayer2Player(){player2Type = PlayerType.player;}
-    public void SetPlayer2MCTS(){player2Type = PlayerType.mcts;}
-    public void SetPlayer2Random(){player2Type = PlayerType.random;}
+    public void SetPlayer1Player(){playerTypes[0] = PlayerType.player;}
+    public void SetPlayer1MCTS(){playerTypes[0] = PlayerType.mcts;}
+    public void SetPlayer1Random(){playerTypes[0] = PlayerType.random;}
+    public void SetPlayer2Player(){playerTypes[1] = PlayerType.player;}
+    public void SetPlayer2MCTS(){playerTypes[1] = PlayerType.mcts;}
+    public void SetPlayer2Random(){playerTypes[1] = PlayerType.random;}
 }
