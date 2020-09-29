5f8078f51bd2222132de01091f9498f44fea0705
Tom <t.rakoto76@gmail.com>
2020-09-29 17:36:59 +0200

    :sparkles: add Random player

---
+++ Valhalla/Assets/Scripts/AController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Character))]
+
+public abstract class AController : MonoBehaviour
+{
+    protected Character _character;
+    protected float _direction = 0;
+    protected bool _isCrouching = false;
+    protected bool _isJumping;
+    protected bool _noSword;
+    protected bool _throwSword = false;
+    protected Thrust _thrust;
+
+    protected abstract void ExecuteActions();
+
+    private void Awake()
+    {
+        _character = GetComponent<Character>();
+        _thrust = Thrust.NONE;
+    }
+    private void Update()
+    {
+        ExecuteActions();
+    }
+    private void FixedUpdate()
+    {
+        _character.Move(_direction,_isCrouching,_isJumping);
+        _isJumping = false;
+        if (_throwSword)
+        {
+            _character.ThrowSword();
+            _noSword = true;
+        }
+        if(_noSword)  return;
+        _character.Thrusting(_thrust);
+        _thrust = Thrust.NONE;
+    }
+}
---
+++ Valhalla/Assets/Scripts/RandomController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomController : AController
+{
+    private int randomInt;
+    protected override void ExecuteActions()
+    {
+        StartCoroutine(ChooseAnAction());
+        
+    }
+
+    private IEnumerator ChooseAnAction()
+    {
+        randomInt = Random.Range(0, 4);
+        switch (randomInt)
+        {
+            case 0:
+            {
+                var r = Random.Range(-1, 2);
+                _direction = r;
+                break;
+            }
+            case 1:
+            {
+                var r = Random.Range(0, 2) != 0;
+                _isJumping = r;
+                break;
+            }
+            case 2:
+            {
+                var r = Random.Range(0, 2) != 0;
+                _isCrouching = Random.Range(0, 2) != 0;
+                break;
+            }
+            case 3:
+            {
+                var r = Random.Range(0, 3);
+                _thrust = (Thrust) r;
+                break;
+            }
+            default: yield return new WaitForSeconds(10);break;
+        }
+
+
+    }
+
+}
+    
+
