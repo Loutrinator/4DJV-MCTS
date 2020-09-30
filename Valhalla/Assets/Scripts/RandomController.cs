5f2b3e43b6141c3640bbeaed444869f53e87f12c
Antoine Pavy <antoine.pavy92@gmail.com>
2020-09-30 16:44:31 +0200

    :fire: Removed ChooseAction in RandomController

--- Valhalla/Assets/Scripts/RandomController.cs
+++ Valhalla/Assets/Scripts/RandomController.cs
@@ -37,45 +37,7 @@ public class RandomController : AController
                 break;
             }
         }
-        
     }
-
-    private IEnumerator ChooseAnAction()
-    {
-        if(_isDead || !GameManager.IsPaused) yield return null;
-        randomInt = Random.Range(0, 4);
-        switch (randomInt)
-        {
-            case 0:
-            {
-                var r = Random.Range(-1, 2);
-                _direction = r;
-                break;
-            }
-            case 1:
-            {
-                var r = Random.Range(0, 2) != 0;
-                _isJumping = r;
-                break;
-            }
-            case 2:
-            {
-                var r = Random.Range(0, 2) != 0;
-                _isCrouching = Random.Range(0, 2) != 0;
-                break;
-            }
-            case 3:
-            {
-                var r = Random.Range(0, 3);
-                _thrust = (Thrust) r;
-                break;
-            }
-            default: yield return new WaitForSeconds(10);break;
-        }
-
-
-    }
-
 }
     
 
