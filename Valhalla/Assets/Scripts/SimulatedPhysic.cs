62e806c7c9a945b2531a002f86ffdf6e4b8d0ed3
Tom <t.rakoto76@gmail.com>
2020-10-01 14:37:35 +0200

    :sparkles: add SimulatedPhysic static class

---
+++ Valhalla/Assets/Scripts/SimulatedPhysic.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class SimulatedPhysic 
+{
+   public static bool BoundsAreIntersecting(Bounds a, Bounds b)
+   {
+      return ((Mathf.Abs(a.min.x - b.min.x) * 2) <(a.size.x + b.size.x))
+             && ((Mathf.Abs(a.min.y - b.min.y) * 2) <(a.size.y + b.size.y));
+   }
+
+   public static Vector3 NextPosition(Vector3 position, Vector3 velocity)
+   {
+        return  position + velocity * Time.deltaTime;
+   }
+}
