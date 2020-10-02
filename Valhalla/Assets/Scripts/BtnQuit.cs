4107a485b54e73381d7713cdb19bb4fd5b369cdd
Tom <t.rakoto76@gmail.com>
2020-10-02 12:57:59 +0200

    :sparkles: add button quit

---
+++ Valhalla/Assets/Scripts/BtnQuit.cs
@@ -0,0 +1,18 @@
+﻿using Button = UnityEngine.UI.Button;
+using UnityEngine;
+
+public class BtnQuit : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener( () =>
+        {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+        });
+    }
+}
