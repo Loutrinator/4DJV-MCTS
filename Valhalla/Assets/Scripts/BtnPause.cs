af66b8e9ce43165a325cee9ffea823654d3e7c36
Tom <t.rakoto76@gmail.com>
2020-10-02 12:58:31 +0200

    :sparkles: add pause screen

--- Valhalla/Assets/Scripts/BtnPause.cs
+++ Valhalla/Assets/Scripts/BtnPause.cs
@@ -3,8 +3,14 @@ using Button = UnityEngine.UI.Button;
 
 public class BtnPause : MonoBehaviour
 {
+    public GameObject PauseGameMenu = null;
     void Start()
     {
-        GetComponent<Button>().onClick.AddListener( GameManager.Instance.PauseGame );
+        PauseGameMenu.SetActive(false);
+        GetComponent<Button>().onClick.AddListener( () =>
+        {
+            GameManager.Instance.PauseGame();
+            PauseGameMenu.SetActive(GameManager.IsPaused);
+        });
     }
 }
